2455bff107c9b6ab5f862c3a65c2d2f545fc7722
Speedy09 <sniadek97@gmail.com>
2020-03-28 16:02:11 +0100

    creating items

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,33 @@
 package sample;
 
+import sample.datamodel.TodoItem;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
+
+    private List<TodoItem> todoItems;
+
+    public void initialize(){
+        TodoItem item1 = new TodoItem("Mail birthday card", "Buy something",
+                LocalDate.of(2020, Month.APRIL, 01));
+        TodoItem item2 = new TodoItem("Mail birthday card", "Buy something",
+                LocalDate.of(2020, Month.APRIL, 15));
+        TodoItem item3 = new TodoItem("Mail birthday card", "Buy something",
+                LocalDate.of(2020, Month.MAY, 03));
+        TodoItem item4 = new TodoItem("Mail birthday card", "Buy something",
+                LocalDate.of(2020, Month.APRIL, 20));
+        TodoItem item5 = new TodoItem("Mail birthday card", "Buy something",
+                LocalDate.of(2020, Month.MAY, 13));
+
+        todoItems = new ArrayList<TodoItem>();
+        todoItems.add(item1);
+        todoItems.add(item2);
+        todoItems.add(item3);
+        todoItems.add(item4);
+        todoItems.add(item5);
+    }
 }
