ebbd0ef99046c42971635d81be7a5106368fbad2
Speedy09 <sniadek97@gmail.com>
2020-03-28 17:35:54 +0100

    write and read items

--- src/sample/datamodel/TodoData.java
+++ src/sample/datamodel/TodoData.java
@@ -1,6 +1,16 @@
 package sample.datamodel;
 
+import javafx.collections.FXCollections;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Iterator;
 import java.util.List;
 
 public class TodoData {
@@ -25,5 +35,51 @@ public class TodoData {
 
     public void setTodoItems(List<TodoItem> todoItems) {
         this.todoItems = todoItems;
+    }
+
+    public void loadTodoItems() throws IOException {
+        todoItems = FXCollections.observableArrayList();
+        Path path = Paths.get(filename);
+        BufferedReader br = Files.newBufferedReader(path);
+
+        String input;
+        try{
+            while((input = br.readLine()) != null){
+                String[] itemPieces = input.split("\t");
+                String description = itemPieces[0];
+                String details = itemPieces[1];
+                String dateString = itemPieces[2];
+
+                LocalDate date = LocalDate.parse(dateString, formatter);
+                TodoItem todoItem = new TodoItem(description, details, date);
+                todoItems.add(todoItem);
+            }
+        } finally {
+            if(br != null){
+                br.close();
+            }
+        }
+    }
+
+    public void storeTodoItems() throws IOException {
+        Path path = Paths.get(filename);
+        BufferedWriter bw = Files.newBufferedWriter(path);
+
+        try {
+            Iterator<TodoItem> iter = todoItems.iterator();
+            while(iter.hasNext()){
+                TodoItem item = iter.next();
+                bw.write(String.format("%s\t%s\t%s",
+                                        item.getDescription(),
+                                        item.getDetails(),
+                                        item.getDeadline().format(formatter)));
+
+                bw.newLine();
+            }
+        } finally {
+            if(bw != null){
+                bw.close();
+            }
+        }
     }
 }
